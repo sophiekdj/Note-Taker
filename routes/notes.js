59d6a3a3bf137817723d4ac1316d86ae63be419d
sophiekdj <s.dejong057@gmail.com>
2022-01-24 13:43:10 +0800

    moved files out of Dev, fixed uuid

---
+++ routes/notes.js
@@ -0,0 +1,37 @@
+const fb = require("express").Router();
+const { readAndAppend, readFromFile } = require("../helpers/fsUtils");
+const { uuid } = require("uuidv4");
+
+// GET Route for retrieving all the feedback
+fb.get("/", (req, res) =>
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
+);
+
+// POST Route for submitting feedback
+fb.post("/", (req, res) => {
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      notes_id: uuid(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+
+    const response = {
+      status: "success",
+      body: newFeedback,
+    };
+
+    res.json(response);
+  } else {
+    res.json("Error in posting feedback");
+  }
+});
+
+module.exports = fb;
